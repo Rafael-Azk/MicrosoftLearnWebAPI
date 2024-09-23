823e07109453fbc91166d28d258bcb0eb73edd6f
Rafael de Almeida <bloodrafa@hotmail.com>
2024-09-23 04:11:29 -0300

    PC_Casa

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+//Db context para a aplicação de funcionalidades do Entity Framework.
+namespace TodoApi.Models;
+
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+    {
+
+    }
+    public DbSet<TodoItem> TodoItems { get; set; }
+}
+
